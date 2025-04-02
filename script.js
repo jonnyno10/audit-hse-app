ab8f7f059955f5d7c3704da0d8fceec3849696a5
jonnyno10 <barontinijonathan@gmail.com>
2025-04-02 19:23:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const CLIENT_ID = 972446330294-nir948gv3j91qa89r3gil7unrc58roi5.apps.googleusercontent.com; // Inserisci qui il tuo Client ID OAuth 2.0
-const API_KEY = AIzaSyA5jmTA54BAziJFzNfFH9Vf3mFen8kTfjM;     // Inserisci qui la tua API Key
+const CLIENT_ID = "972446330294-l5mgbh2q5v1fo5aor6pe5k0de22n6282.apps.googleusercontent.com"; // Inserisci qui il tuo Client ID OAuth 2.0
+const API_KEY = "AIzaSyA5jmTA54BAziJFzNfFH9Vf3mFen8kTfjM";     // Inserisci qui la tua API Key
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 const SCOPES = "https://www.googleapis.com/auth/drive.readonly";
 
