651fc18b7ccb36ed65a05ea7114e92034ee1d709
jonnyno10 <barontinijonathan@gmail.com>
2025-04-02 17:23:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 let startX, startY;
+let isDragging = false;
+
+function disableScroll() {
+    document.body.style.overflow = "hidden";
+}
+
+function enableScroll() {
+    document.body.style.overflow = "auto";
+}
 
 function handleTouchStart(event) {
     const touch = event.touches[0];
     startX = touch.clientX;
     startY = touch.clientY;
+    isDragging = false;
 }
 
 function handleTouchMove(event, row) {
-    event.preventDefault(); // Evita lo scrolling accidentale
     const touch = event.touches[0];
     const statusCell = row.cells[2];
     const movementX = touch.clientX - startX;
     const movementY = touch.clientY - startY;
+
+    if (Math.abs(movementX) > 10 || Math.abs(movementY) > 10) {
+        isDragging = true;
+        disableScroll(); // Disabilita lo scroll durante il trascinamento
+    }
+
+    event.preventDefault(); // Evita lo scrolling accidentale
 
     // Determina la direzione del movimento e mostra anteprima del colore e del testo
     if (Math.abs(movementX) > Math.abs(movementY)) {
@@ -34,6 +50,10 @@ function handleTouchMove(event, row) {
 }
 
 function handleTouchEnd(event, row) {
+    enableScroll(); // Riattiva lo scroll alla fine del trascinamento
+    
+    if (!isDragging) return;
+    
     const touch = event.changedTouches[0];
     const statusCell = row.cells[2];
     const movementX = touch.clientX - startX;
